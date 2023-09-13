541a4bcef75d81410dee402d91354de777332ee0
savar <savar@oasis.localdomain>
2023-09-13 15:29:18 +0530

    some changes

--- pome/.config/2bwm/autostart.sh
+++ pome/.config/2bwm/autostart.sh
@@ -11,6 +11,7 @@ if hash polybar >/dev/null 2>&1; then
 	setsid -f polybar battery &
 	setsid -f polybar tags &
 	setsid -f polybar search &
+	#setsid -f polybar weather &
 	#sleep 1 && setsid -f polybar dummy &
 	sleep 1 && setsid -f polybar dummy && sleep 1 && polybar-msg -p $(xdotool search --name dummy getwindowpid) cmd toggle &
 fi
