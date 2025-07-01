5f63f848db2b6bbfceca54f81bdff8b601bb065e
savar <skaar513@gmail.com>
2025-07-02 03:46:01 +0530

    configs

--- configs/.config/dwm/scripts/battery.sh
+++ configs/.config/dwm/scripts/battery.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source ~/.config/dwm/scripts/vars.sh
-echo "^c$BLUE^ 82"
+echo "^c$BLUE^$(sb_battery icon) $(sb_battery perc)"
+sb_battery notify_critical &
 
 case "$BLOCK_BUTTON" in
 	1) notify-send -i ~/pix/assets/icons/battery-full-solid.svg -a scripts -r 13 "battery" "$(sb_battery perc)" ;;
